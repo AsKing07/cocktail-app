64c0c74f1deda2cda71704592a419a072a1c55b9
Charbel SONON <charbelsnn@gmail.com>
2025-05-07 11:44:20 +0200

    bugfix/loading behavior in cocktail list component

--- src/app/cocktail-list/cocktail-list.component.ts
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Cocktail } from '../models/cocktail.model';
 import { CocktailService } from '../services/cocktail.service';
 import { CocktailComponent } from '../cocktail/cocktail.component';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import {  Subject, takeUntil } from 'rxjs';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import {faFaceSadTear} from '@ng-icons/font-awesome/regular';
 
@@ -17,39 +17,46 @@ import {faFaceSadTear} from '@ng-icons/font-awesome/regular';
 })
 export class CocktailListComponent implements OnInit, OnDestroy {
   cocktails: Cocktail[] = [];
+  initialized = false;
   state = {
     loading: true,
     error: '',
   };
 private destroy$ = new Subject<boolean>();
 
-  constructor(private cocktailService: CocktailService) {}
+  constructor(private cocktailService: CocktailService) {
+    this.state.loading = true;
+    this.state.error = '';
+    
+    this.cocktails = [];
+  }
 
   ngOnInit(): void {
+    this.state.loading = true;
     this.cocktails = [];
     this.loadCocktails();
+    this.initialized = true; 
   }
 
   loadCocktails(): void {
     this.state.loading = true;
     this.state.error = '';
     this.cocktailService.getRandomCocktails().pipe(
-      takeUntil(this.destroy$) // Désinscription de l'observable lorsque le composant est détruit
+      takeUntil(this.destroy$)
     ).subscribe({
       next: (data) => {
+        this.state.loading = false;
         this.cocktails = data;
-        setTimeout(() => {
-          this.state.loading = false;
-        }, 1000);
-        
+      
+
       },
       error: (error) => {
         this.state.error = `Erreur lors de la récupération des cocktails : ${error.message}`;
         this.state.loading = false;
+  
       },
     });
   }
-
   onRefresh(): void {
     this.loadCocktails();
   }
